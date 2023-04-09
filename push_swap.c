9e6b45eb8aa02fcf5515d0dbe2d983d3b3e5da4c
Michael Rizhakov <m.rizhakov@gmail.com>
2023-04-09 21:17:36 +0200

    Reworked to change struct to linked lists. Now we can add the stack from user input to a linked list, print out it's contents for testing and free memory on exit

--- push_swap.c
+++ push_swap.c
@@ -14,28 +14,50 @@
 
 int		main(int argc, char **argv)
 {
-    t_stack		stk_a;
-    stk_a.size = argc - 1;
+	int i = 1;
+	t_stack *stk_a;
+	stk_a = NULL;
 
-    stk_a.stack = malloc(sizeof(int) * argc);
-    if(argc < 2) {
-        write(1,"you need more arguments", 23);
-        return (1);
-    }
-    stk_a.i = 0;
-    while(stk_a.i < stk_a.size)
-    if(ft_isdigit2(argv[stk_a.i + 1]) == -1) {
-        write(1, "Error", 5);
-        exit(1);
-    }
-    else
-    {
-        stk_a.stack[stk_a.i] = ft_atoi(argv[stk_a.i + 1]);
-        stk_a.i++;
-    }
+	while (i != argc)
+	{
+		ft_lstadd_back(&stk_a, ft_lstnew(atoi(argv[i])));
+		i++;
+	}
+	print_list(stk_a);
+	free_list(stk_a);
+	return (0);
 
-        for(int i = 0; i < stk_a.size;i++)
-            printf("%d\n", stk_a.stack[i]);
-    free(stk_a.stack);
-    return (0);
+//    t_stack		stk_a;
+////	stk_a.size = argc - 1;
+//
+//	stk_a = NULL;
+//
+//    stk_a.stack = malloc(sizeof(int) * argc);
+//    if(argc < 2) {
+//        write(1,"you need more arguments", 23);
+//        return (1);
+//    }
+//    stk_a.i = 0;
+//    while(stk_a.i < stk_a.size)
+//    if(ft_isdigit2(argv[stk_a.i + 1]) == -1) {
+//        write(1, "Error", 5);
+//        exit(1);
+//    }
+//    else
+//    {
+//		if (stk_a)
+//		{
+//			ft_lstnew(ft_atoi(argv[stk_a.i + 1]);
+//		}
+//		else
+//		{
+//		    ft_lstadd_back(*stk_a.stack, ft_atoi(argv[stk_a.i + 1]);
+//		}
+//        stk_a.i++;
+//    }
+//
+//        for(int i = 0; i < stk_a.size;i++)
+//            printf("%d\n", stk_a.stack[i]);
+//    free(stk_a.stack);
+//    return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -10,22 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_stack {
+	int content;
+	struct s_stack *next;
+} t_stack;
+
+//
+//typedef struct s_stack
+//{
+//	int *stack;
+//	struct s_stack *next;
+//	int size;
+//	int i;
+//} t_stack;
+
+/*
 typedef struct s_stack
 {
     int *stack;
     int size;
     int i;
-}t_stack;
+}t_stack;*/
 
-int	ft_isdigit(int c);
-int ft_isdigit2(const char *str);
-int	ft_atoi(const char *str);
+int     ft_isdigit(int c);
+int     ft_isdigit2(const char *str);
+int     ft_atoi(const char *str);
+void    ft_lstadd_back(t_stack **lst, t_stack *new);
+void    ft_lstadd_front(t_stack **lst, t_stack *new);
+void	ft_lstclear(t_stack **lst, void (*del)(void *));
+void	ft_lstdelone(t_stack *lst, void (*del)(void *));
+void	ft_lstiter(t_stack *lst, void (*f)(void *));
+t_stack	*ft_lstlast(t_stack *lst);
+t_stack	*ft_lstmap(t_stack *lst, void *(*f)(void *), void (*del)(void *));
+t_stack	*ft_lstnew(int content);
+int	    ft_lstsize(t_stack *lst);
+void    free_list(t_stack *head);
+void    print_list(t_stack *head);
+
+
 
 #endif //PUSH_SWAP_H
